e5ec1c1c53ffb8da17ba6f98c1e0956cb5914804
Kerim Cetinbas <a.cetinbas@icloud.com>
2025-09-11 18:39:31 +0300

    chore(examples): WeatherForecasts example added

---
+++ examples/WeatherForecastExample/Queries/WeatherForecastQuery.cs
@@ -0,0 +1,6 @@
+﻿using Ergosfare.Contracts;
+using WeatherForecastExample.Entities;
+
+namespace WeatherForecastExample.Queries;
+
+public record WeatherForecastQuery(byte Amount) : IQuery<WeatherForecast[]>;
