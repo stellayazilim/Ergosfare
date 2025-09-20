7af6b886f98142f7aeed9801075155b3cd08e86e
Kerim Cetinbas <a.cetinbas@icloud.com>
2025-09-20 04:40:06 +0300

    feat(ExecutionContext) added cleanest way to get and set ambient data

--- src/Ergosfare.Context/IExecutionContext.cs
+++ src/Ergosfare.Context/IExecutionContext.cs
@@ -13,6 +13,44 @@
 /// </remarks>
 public interface IExecutionContext
 {
+
+    /// <summary>
+    /// Stores an item in the execution context associated with the specified key.
+    /// </summary>
+    /// <param name="key">The key used to identify the item.</param>
+    /// <param name="item">The object to store in the context.</param>
+    void Set(string key, object item);
+    
+    
+    /// <summary>
+    /// Determines whether an item with the specified key exists in the execution context.
+    /// </summary>
+    /// <param name="key">The key to check for existence.</param>
+    /// <returns><c>true</c> if an item with the key exists; otherwise, <c>false</c>.</returns>
+    bool Has(string key);
+    
+    
+    /// <summary>
+    /// Tries to retrieve an item of type <typeparamref name="TType"/> from the execution context.
+    /// </summary>
+    /// <typeparam name="TType">The expected type of the item.</typeparam>
+    /// <param name="key">The key associated with the item.</param>
+    /// <param name="item">When this method returns, contains the retrieved item if found; otherwise, the default value of <typeparamref name="TType"/>.</param>
+    /// <returns><c>true</c> if the item was found and is of type <typeparamref name="TType"/>; otherwise, <c>false</c>.</returns>
+    bool TryGet<TType>(string key, out TType item);
+    
+    
+    /// <summary>
+    /// Retrieves an item of type <typeparamref name="TType"/> from the execution context.
+    /// </summary>
+    /// <typeparam name="TType">The expected type of the item.</typeparam>
+    /// <param name="key">The key associated with the item.</param>
+    /// <returns>The item associated with the specified key.</returns>
+    /// <exception cref="KeyNotFoundException">Thrown if no item exists with the specified key.</exception>
+    /// <exception cref="InvalidCastException">Thrown if the item is not of type <typeparamref name="TType"/>.</exception>
+    TType Get<TType>(string key) where TType : notnull;
+    
+    
     /// <summary>
     ///     Gets the cancellation token associated with the execution context.
     /// </summary>
@@ -21,6 +59,7 @@ public interface IExecutionContext
     ///     Handlers should periodically check this token and abort their execution if cancellation is requested.
     /// </remarks>
     CancellationToken CancellationToken { get; }
+    
 
     /// <summary>
     ///     Gets a key/value collection that can be used to share data within the scope of this execution.
@@ -31,8 +70,8 @@ public interface IExecutionContext
     ///     different mediation operations.
     /// </remarks>
     IDictionary<object, object?> Items { get; }
-
-
+    
+    
     /// <summary>
     ///     The result of the message mediation.
     /// </summary>
@@ -42,7 +81,8 @@ public interface IExecutionContext
     ///     in certain scenarios, such as when aborting the execution.
     /// </remarks>
     object? MessageResult { get; set; }
-
+    
+    
     /// <summary>
     ///     Aborts the execution of the current mediation execution.
     /// </summary>
@@ -57,4 +97,7 @@ public interface IExecutionContext
     ///     a message result must be provided to satisfy the result type requirement.
     /// </remarks>
     void Abort(object? messageResult = null);
+    
+    
+    
 }
